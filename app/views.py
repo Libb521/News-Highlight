eed39c0569d96788f396ca2c7214e3de77c63946
Libb521 <eotieno39@yahoo.com>
2020-02-03 16:54:01 +0300

    created three classed

--- app/views.py
+++ app/views.py
@@ -6,6 +6,14 @@ from app import app
 def index():
 
     '''
-    view root page function that returns the index page and its data
+    View root page function that returns the index page and its data
     '''
+    
     return render_template('index.html')
+@app.route('/news/<news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
