9602fe57a41bc9ac885be1a4e5c90d28207ff0e6
Libb521 <eotieno39@yahoo.com>
2020-02-03 22:12:23 +0300

    Added the News API key into the config.py fil

--- app/views.py
+++ app/views.py
@@ -8,12 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    
-    return render_template('index.html')
+    title = "For The Latest and Newest News Articles"
+    return render_template('index.html',title = title)
+
 @app.route('/news/<news_id>')
 def news(news_id):
 
     '''
     View news page function that returns the news details page and its data
     '''
-    return render_template('news.html',id = news_id)
+    title = f'You are viewing {news_id}'
+    return render_template('news.html',title = title)
